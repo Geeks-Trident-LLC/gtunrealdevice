90e02a119c47a2f5dd5bb05bacbf0c51c544ccbf
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-01-26 16:23:53 -0800

    core: declare class and methods in __init__.py

--- gtunrealdevice/__init__.py
+++ gtunrealdevice/__init__.py
@@ -1,6 +1,12 @@
 """Top-level module for gtunrealdevice.
 
 """
+from gtunrealdevice.core import URDevice
+from gtunrealdevice.core import create
+from gtunrealdevice.core import connect
+from gtunrealdevice.core import disconnect
+from gtunrealdevice.core import execute
+from gtunrealdevice.core import configure
 
 from gtunrealdevice.config import version
 from gtunrealdevice.config import edition
@@ -9,6 +15,12 @@ __version__ = version
 __edition__ = edition
 
 __all__ = [
+    'URDevice',
+    'create',
+    'connect',
+    'disconnect',
+    'execute',
+    'configure',
     'version',
     'edition',
 ]
