1dbbbbb8c60222cc44abf5b0141ae1056b75dfff
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-03-24 09:28:53 -0700

    baseline: rework utils.py

--- gtunrealdevice/utils.py
+++ gtunrealdevice/utils.py
@@ -1,20 +1,22 @@
 """Module containing the logic for utilities."""
 
-import re
 from pathlib import Path
 from pathlib import PurePath
 from datetime import datetime
+
+from textwrap import wrap
 
 
 class Printer:
     """A printer class.
 
     Methods
-    Printer.get(data, header='', footer='', failure_msg='', width=80) -> str
-    Printer.print(data, header='', footer='', failure_msg='', print_func=None) -> None
+    Printer.get(data, header='', footer='', failure_msg='', width=80, width_limit=20) -> str
+    Printer.print(data, header='', footer='', failure_msg='', width=80, width_limit=20, print_func=None) -> None
     """
     @classmethod
-    def get(cls, data, header='', footer='', width=80, failure_msg=''):
+    def get(cls, data, header='', footer='',
+            width=80, width_limit=20, failure_msg=''):
         """Decorate data by organizing header, data, footer, and failure_msg
 
         Parameters
@@ -23,36 +25,28 @@ class Printer:
         header (str): a header text.  Default is empty.
         footer (str): a footer text.  Default is empty.
         width (int): width of displayed text.  Default is 80.
+        width_limit (int): minimum width of displayed text.  Default is 20.
         failure_msg (str): a failure message.  Default is empty.
         """
         headers = str(header).splitlines()
         footers = str(footer).splitlines()
-        data = data if isinstance(data, (list, tuple)) else [data]
+        data = data if Misc.is_sequence_instance(data) else [data]
         lst = []
         result = []
 
-        left_limit = 20
-        left_bound, right_bound = left_limit - 4, width - 4
-
-        if width > left_limit:
-            pat = r'(.{%s,%s}\S) +' % (left_bound, right_bound)
-        else:
-            pat = ''
+        right_bound = width - 4
 
         for item in data:
-            if pat:
+            if width >= width_limit:
                 for line in str(item).splitlines():
-                    line = line.rstrip()
-                    if len(line) > right_bound:
-                        line = re.sub(pat, r'\1\n', line)
-                        lst.extend(line.splitlines())
-                    else:
-                        lst.append(line)
+                    lst.extend(wrap(line, width=right_bound))
             else:
                 lst.extend(line.rstrip() for line in str(item).splitlines())
         length = max(len(str(i)) for i in lst + headers + footers)
-        if width > left_limit:
+
+        if width >= width_limit:
             length = right_bound if right_bound > length else length
+
         result.append('+-{}-+'.format('-' * length))
         if header:
             for item in headers:
@@ -75,8 +69,8 @@ class Printer:
         return txt
 
     @classmethod
-    def print(cls, data, header='', footer='', width=80, failure_msg='',
-              print_func=None):
+    def print(cls, data, header='', footer='',
+              width=80, width_limit=20, failure_msg='', print_func=None):
         """Decorate data by organizing header, data, footer, and failure_msg
 
         Parameters
@@ -85,12 +79,14 @@ class Printer:
         header (str): a header text.  Default is empty.
         footer (str): a footer text.  Default is empty.
         width (int): width of displayed text.  Default is 80.
+        width_limit (int): minimum width of displayed text.  Default is 20.
         failure_msg (str): a failure message.  Default is empty.
         print_func (function): a print function.  Default is None.
         """
 
         txt = Printer.get(data, header=header, footer=footer,
-                          failure_msg=failure_msg, width=width)
+                          failure_msg=failure_msg, width=width,
+                          width_limit=width_limit)
 
         print_func = print_func if callable(print_func) else print
         print_func(txt)
@@ -115,17 +111,19 @@ class File:
         return file_obj.exists()
 
     @classmethod
-    def create(cls, filename):
+    def create(cls, filename, showed=True):
         """Check file existence
 
         Parameters
         ----------
         filename (str): a file name
+        showed (bool): showing the message of creating file
 
         Returns
         -------
         bool: True if created, otherwise False
         """
+        filename = cls.get_path(str(filename).strip())
         if cls.is_exist(filename):
             cls.message = 'File is already existed.'
             return True
@@ -136,7 +134,7 @@ class File:
                 file_obj.parent.mkdir(parents=True, exist_ok=True)
             file_obj.touch()
             fmt = '{:%Y-%m-%d %H:%M:%S.%f} - {} file is created.'
-            print(fmt.format(datetime.now(), filename))
+            showed and print(fmt.format(datetime.now(), filename))
             cls.message = '{} file is created.'.format(filename)
             return True
         except Exception as ex:
@@ -158,4 +156,67 @@ class File:
         """
         lst = [Path.home()] if is_home else []
         lst.extend(list(args))
-        return str(PurePath(*lst))
+        file_path = str(Path(PurePath(*lst)).expanduser().absolute())
+        return file_path
+
+    @classmethod
+    def save(cls, filename, data):
+        """Create a file path
+
+        Parameters
+        ----------
+        filename (str): filename
+        data (str): data.
+
+        Returns
+        -------
+        bool: True if successfully saved, otherwise, False
+        """
+        try:
+            if Misc.is_list_instance(data):
+                content = '\n'.join(str(item) for item in data)
+            else:
+                content = str(data)
+
+            filename = cls.get_path(filename)
+            if not cls.create(filename):
+                return False
+
+            file_obj = Path(filename)
+            file_obj.touch()
+            file_obj.write_text(content)
+            cls.message = 'Successfully saved data to "{}" file'.format(filename)
+            return True
+        except Exception as ex:
+            cls.message = '{}: {}'.format(type(ex).__name__, ex)
+            return False
+
+
+class Misc:
+    @classmethod
+    def is_dict_instance(cls, obj):
+        return isinstance(obj, dict)
+
+    @classmethod
+    def is_list_instance(cls, obj):
+        return isinstance(obj, list)
+
+    @classmethod
+    def is_sequence_instance(cls, obj):
+        return isinstance(obj, (list, tuple, set))
+
+    @classmethod
+    def is_integer(cls, obj):
+        return isinstance(obj, int)
+
+    @classmethod
+    def is_boolean(cls, obj):
+        return isinstance(obj, bool)
+
+    @classmethod
+    def is_float(cls, obj):
+        return isinstance(obj, float)
+
+    @classmethod
+    def is_string(cls, obj):
+        return isinstance(obj, str)
