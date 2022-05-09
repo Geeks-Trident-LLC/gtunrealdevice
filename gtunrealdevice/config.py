9f8e1758d52293533020f4b7c04c049e1db40a1a
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-05-09 13:07:21 -0700

    version: update gtunrealdevice to version 0.2.8

--- gtunrealdevice/config.py
+++ gtunrealdevice/config.py
@@ -7,7 +7,7 @@ from textwrap import dedent
 
 from gtunrealdevice.utils import File
 
-__version__ = '0.2.6'
+__version__ = '0.2.8'
 version = __version__
 __edition__ = 'Community'
 edition = __edition__
