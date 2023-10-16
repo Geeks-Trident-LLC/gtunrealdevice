26d53005ff7afef75d515b527e85171ab8afce48
Tuyen Mathew Duong <82467450+geekstrident@users.noreply.github.com>
2023-10-16 11:17:15 -0700

    Update config.py

--- gtunrealdevice/config.py
+++ gtunrealdevice/config.py
@@ -7,7 +7,7 @@ from textwrap import dedent
 
 from gtunrealdevice.utils import File
 
-__version__ = '0.2.8'
+__version__ = '0.2.9'
 version = __version__
 __edition__ = 'Community'
 edition = __edition__
